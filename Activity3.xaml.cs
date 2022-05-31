1d57d4a9dec47e39c17b3892499cde19e0645239
Rxndb <104422620+Rxndb@users.noreply.github.com>
2022-05-31 21:30:55 +0800

    Add files via upload

---
+++ Activity3.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MOD4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity3 : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+
+        public Activity3()
+        {
+            InitializeComponent();
+            EmployeeView.ItemsSource = employees;
+
+            employees.Add(new Employee { DisplayName = "Juan Dela Cruz", position = "President", Image = "image1.jpg" });
+            employees.Add(new Employee { DisplayName = "John Dee", position = "Vice President", Image = "image2.jpg" });
+            employees.Add(new Employee { DisplayName = "Juan Dela Cruz", position = "President", Image = "image1.jpg" });
+            employees.Add(new Employee { DisplayName = "John Dee", position = "Vice President", Image = "image2.jpg" });
+            employees.Add(new Employee { DisplayName = "Juan Dela Cruz", position = "President", Image = "image1.jpg" });
+        }
+    }
+}
